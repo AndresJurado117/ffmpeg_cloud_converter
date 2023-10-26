5e269f8d29f4e6e2182c1cb06dea96b8fa7bbd36
XJuanRocksX <89615507+XJuanRocksX@users.noreply.github.com>
2023-10-26 15:16:08 -0500

    Change GUI, added more functions.

--- web_video/video_convert/static/script.js
+++ web_video/video_convert/static/script.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    /*  Declare Video Mode and Sliders */
     var videoMode = document.getElementById("id_video_mode");
-
     var qpSlider = document.getElementById("id_video_qp");
+    var bitrateSlider = document.getElementById("id_video_bitrate");
+
+    videoMode.onchange = function () {
+        if (this.value == "qp") {
+            qpSlider.disabled = false;
+            bitrateSlider.disabled = true;
+        }
+        else if (this.value == "vbr") {
+            qpSlider.disabled = true;
+            bitrateSlider.disabled = false;
+        }
+    }
+
+    /* QP Slider */
+
     var qpValue = document.getElementById("qp_value");
 
     qpValue.innerHTML = qpSlider.value;
@@ -10,7 +25,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
         qpValue.innerHTML = this.value;
     }
 
-    var bitrateSlider = document.getElementById("id_video_bitrate");
+    /* Video Bitrate Slider */
+
     var bitrateValue = document.getElementById("bitrate_value");
 
     bitrateValue.innerHTML = bitrateSlider.value;
@@ -19,6 +35,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
         bitrateValue.innerHTML = this.value;
     }
 
+    /* Audio Bitrate Slider */
+
     var audioSlider = document.getElementById("id_audio_bitrate");
     var audioValue = document.getElementById("audio_value");
 
@@ -26,5 +44,233 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     audioSlider.oninput = function () {
         audioValue.innerHTML = this.value;
+    }
+
+    /* Brightness Slider */
+
+    var brightnessBoolean = document.getElementById("id_video_brightness_boolean");
+    var brightnessSlider = document.getElementById("id_video_brightness_value");
+    var brightnessValue = document.getElementById("brightness_value");
+
+    brightnessBoolean.onclick = function () {
+        if (brightnessSlider.disabled == true) {
+            brightnessSlider.disabled = false;
+        }
+        else {
+            brightnessSlider.disabled = true;
+        }
+    }
+
+    brightnessValue.innerHTML = parseFloat(brightnessSlider.value).toFixed(2);
+
+    brightnessSlider.oninput = function () {
+        brightnessValue.innerHTML = parseFloat(this.value).toFixed(2);
+    }
+
+    /* Contrast Slider */
+
+    var contrastBoolean = document.getElementById("id_video_contrast_boolean")
+    var contrastSlider = document.getElementById("id_video_contrast_value");
+    var contrastValue = document.getElementById("contrast_value");
+
+    contrastBoolean.onclick = function () {
+        if (contrastSlider.disabled == true) {
+            contrastSlider.disabled = false;
+        }
+        else {
+            contrastSlider.disabled = true;
+        }
+    }
+
+    contrastValue.innerHTML = parseFloat(contrastSlider.value).toFixed(2);
+
+    contrastSlider.oninput = function () {
+        contrastValue.innerHTML = parseFloat(this.value).toFixed(2);
+    }
+
+    /* Saturation Slider */
+
+    var saturationBoolean = document.getElementById("id_video_saturation_boolean")
+    var saturationSlider = document.getElementById("id_video_saturation_value");
+    var saturationValue = document.getElementById("saturation_value");
+
+    saturationBoolean.onclick = function () {
+        if (saturationSlider.disabled == true) {
+            saturationSlider.disabled = false;
+        }
+        else {
+            saturationSlider.disabled = true;
+        }
+    }
+
+    saturationValue.innerHTML = parseFloat(saturationSlider.value).toFixed(2);
+
+    saturationSlider.oninput = function () {
+        saturationValue.innerHTML = parseFloat(this.value).toFixed(2);
+    }
+
+    /* Volumen Slider */
+
+    var volumeSlider = document.getElementById("id_audio_volume");
+    var volumeValue = document.getElementById("volume_value");
+
+    volumeValue.innerHTML = Math.round(volumeSlider.value * 100);
+
+    volumeSlider.oninput = function () {
+        volumeValue.innerHTML = Math.round(this.value * 100);
+    }
+
+    /* EQ Presets and Sliders */
+
+    var eqPresets = document.getElementById("id_equalizer_preset");
+
+    var eqBarOneSlider = document.getElementById("id_eq_ten_band_1");
+    var eqBarOneValue = document.getElementById("eq_bar_1");
+
+    eqBarOneValue.innerHTML = eqBarOneSlider.value;
+
+    eqBarOneSlider.oninput = function () {
+        eqBarOneValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarTwoSlider = document.getElementById("id_eq_ten_band_2");
+    var eqBarTwoValue = document.getElementById("eq_bar_2");
+
+    eqBarTwoValue.innerHTML = eqBarTwoSlider.value;
+
+    eqBarTwoSlider.oninput = function () {
+        eqBarTwoValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarThreeSlider = document.getElementById("id_eq_ten_band_3");
+    var eqBarThreeValue = document.getElementById("eq_bar_3");
+
+    eqBarThreeValue.innerHTML = eqBarThreeSlider.value;
+
+    eqBarThreeSlider.oninput = function () {
+        eqBarThreeValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarFourSlider = document.getElementById("id_eq_ten_band_4");
+    var eqBarFourValue = document.getElementById("eq_bar_4");
+
+    eqBarFourValue.innerHTML = eqBarFourSlider.value;
+
+    eqBarFourSlider.oninput = function () {
+        eqBarFourValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarFiveSlider = document.getElementById("id_eq_ten_band_5");
+    var eqBarFiveValue = document.getElementById("eq_bar_5");
+
+    eqBarFiveValue.innerHTML = eqBarFiveSlider.value;
+
+    eqBarFiveSlider.oninput = function () {
+        eqBarFiveValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarSixSlider = document.getElementById("id_eq_ten_band_6");
+    var eqBarSixValue = document.getElementById("eq_bar_6");
+
+    eqBarSixValue.innerHTML = eqBarSixSlider.value;
+
+    eqBarSixSlider.oninput = function () {
+        eqBarSixValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarSevenSlider = document.getElementById("id_eq_ten_band_7");
+    var eqBarSevenValue = document.getElementById("eq_bar_7");
+
+    eqBarSevenValue.innerHTML = eqBarSevenSlider.value;
+
+    eqBarSevenSlider.oninput = function () {
+        eqBarSevenValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarEightSlider = document.getElementById("id_eq_ten_band_8");
+    var eqBarEightValue = document.getElementById("eq_bar_8");
+
+    eqBarEightValue.innerHTML = eqBarEightSlider.value;
+
+    eqBarEightSlider.oninput = function () {
+        eqBarEightValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarNineSlider = document.getElementById("id_eq_ten_band_9");
+    var eqBarNineValue = document.getElementById("eq_bar_9");
+
+    eqBarNineValue.innerHTML = eqBarNineSlider.value;
+
+    eqBarNineSlider.oninput = function () {
+        eqBarNineValue.innerHTML = this.value;
+        eqPresets.value = "custom"
+    }
+
+    var eqBarTenSlider = document.getElementById("id_eq_ten_band_10");
+    var eqBarTenValue = document.getElementById("eq_bar_10");
+
+    eqBarTenValue.innerHTML = eqBarTenSlider.value;
+
+    eqBarTenSlider.oninput = function () {
+        eqBarTenValue.innerHTML = this.value;
+        eqPresets.value = "custom";
+    }
+
+    function equalizerValues(bandOne, bandTwo, bandThree, bandFour, bandFive, bandSix, bandSeven, bandEight, bandNine, bandTen) {
+        eqBarOneSlider.value = eqBarOneValue.value = eqBarOneValue.innerHTML = bandOne;
+        eqBarTwoSlider.value = eqBarTwoValue.value = eqBarTwoValue.innerHTML = bandTwo;
+        eqBarThreeSlider.value = eqBarThreeValue.value = eqBarThreeValue.innerHTML = bandThree;
+        eqBarFourSlider.value = eqBarFourValue.value = eqBarFourValue.innerHTML = bandFour;
+        eqBarFiveSlider.value = eqBarFiveValue.value = eqBarFiveValue.innerHTML = bandFive;
+        eqBarSixSlider.value = eqBarSixValue.value = eqBarSixValue.innerHTML = bandSix;
+        eqBarSevenSlider.value = eqBarSevenValue.value = eqBarSevenValue.innerHTML = bandSeven;
+        eqBarEightSlider.value = eqBarEightValue.value = eqBarEightValue.innerHTML = -bandEight;
+        eqBarNineSlider.value = eqBarNineValue.value = eqBarNineValue.innerHTML = bandNine;
+        eqBarTenSlider.value = eqBarTenValue.value = eqBarTenValue.innerHTML = bandTen;
+    }
+
+    eqPresets.onchange = function () {
+        /*
+        switch (this.value) {
+            case "eq_hi_end":
+                equalizerValues(-15, -5, -1, -1, 0, 0.5, 2, 1.5, 3, 4.2)
+            case "eq_dialog_female_lav":
+                equalizerValues(-10, -0.5, 2.7, 2.5, 1, 0, 1, 3, 4.5, 4.5)
+            case "eq_dialog_male_lav":
+                equalizerValues(-10, 0, 3.8, -20, 5.1, 2, 0.5, 0.5, 2.5, 4)
+            case "eq_dialog_male":
+                equalizerValues(-20, -12, -1, 0.5, -5, 2, 4, 1.5, -2, 2)
+            case "eq_telephone":
+                equalizerValues(-20, -20, -20, -20, 0, 10, 0, -20, -20, -20)
+            case "eq_music_top_end_boost":
+                equalizerValues(0, 0, 0, 0, 0, 0, 0, 2, 4, 4)
+        }
+        */
+        if (this.value == "eq_hi_end") {
+            equalizerValues(-15, -5, -1, -1, 0, 0.5, 2, 1.5, 3, 4.2)
+        }
+        else if (this.value == "eq_dialog_female_lav") {
+            equalizerValues(-10, -0.5, 2.7, 2.5, 1, 0, 1, 3, 4.5, 4.5)
+        }
+        else if (this.value == "eq_dialog_male_lav") {
+            equalizerValues(-10, 0, 3.8, -20, 5.1, 2, 0.5, 0.5, 2.5, 4)
+        }
+        else if (this.value == "eq_dialog_male") {
+            equalizerValues(-20, -12, -1, 0.5, -5, 2, 4, 1.5, -2, 2)
+        }
+        else if (this.value == "eq_telephone") {
+            equalizerValues(-20, -20, -20, -20, 0, 10, 0, -20, -20, -20)
+        }
+        else if (this.value == "eq_music_top_end_boost") {
+            equalizerValues(0, 0, 0, 0, 0, 0, 0, 2, 4, 4)
+        }
     }
 });
--- web_video/video_convert/views.py
+++ web_video/video_convert/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 import ffmpeg, requests
 
+# Nvidia RTX 30/40 Series supports 5 simultaneous encoding sessions, apparently RTX A2000, A4000 can take around 26 sessions
+SIMULTANEOUS_TRANSCODING_SESSIONS = 5
+
 # Set global values for audio and video encoding
 VIDEO_QP_MIN = 20
 VIDEO_QP_MAX = 32
@@ -14,6 +17,13 @@ VIDEO_BITRATE_INITIAL = 2500
 AUDIO_BITRATE_MIN = 32
 AUDIO_BITRATE_MAX = 256
 AUDIO_BITRATE_INITIAL = 128
+
+# Video filter values
+
+# Audio filter values
+AUDIO_VOLUME_MIN = 0.0
+AUDIO_VOLUME_MAX = 4.0
+AUDIO_VOLUME_INITIAL = 1.0
 
 
 class ConvertVideo(forms.Form):
@@ -33,30 +43,198 @@ class ConvertVideo(forms.Form):
     nvidia_rate_control = ("qp", "QP"), ("vbr", "VBR")
 
     input_file = forms.FileField()
+
     video_resolution = forms.ChoiceField(choices=widescreen_resolutions)
+
     video_mode = forms.ChoiceField(choices=nvidia_rate_control)
+
     video_qp = forms.IntegerField(
         min_value=VIDEO_QP_MIN,
         max_value=VIDEO_QP_MAX,
         initial=VIDEO_QP_INITIAL,
         widget=forms.NumberInput(attrs={"type": "range"}),
     )
+
     video_bitrate = forms.IntegerField(
         min_value=VIDEO_BITRATE_MIN,
         max_value=VIDEO_BITRATE_MAX,
         initial=VIDEO_BITRATE_INITIAL,
-        widget=forms.NumberInput(attrs={"type": "range"}),
+        disabled=True,
+        widget=forms.NumberInput(attrs={"type": "range", "step": 100}),
     )
+
     video_preset = forms.ChoiceField(choices=nvidia_presets, initial="medium")
+
     audio_bitrate = forms.IntegerField(
         min_value=AUDIO_BITRATE_MIN,
         max_value=AUDIO_BITRATE_MAX,
         initial=AUDIO_BITRATE_INITIAL,
-        widget=forms.NumberInput(attrs={"type": "range"}),
+        widget=forms.NumberInput(attrs={"type": "range", "step": 32}),
     )
+
     video_flip = forms.BooleanField(required=False)
+
     green_outlines = forms.BooleanField(required=False)
+
+    frame_interpolation = forms.BooleanField(required=False)
+
     gaussian_blur = forms.BooleanField(required=False)
+
+    video_brightness_boolean = forms.BooleanField(required=False)
+
+    video_brightness_value = forms.FloatField(
+        min_value=-1.0,
+        max_value=1.0,
+        initial=0.0,
+        disabled=True,
+        widget=forms.NumberInput(attrs={"type": "range", "step": 0.01}),
+    )
+
+    video_contrast_boolean = forms.BooleanField(required=False)
+
+    video_contrast_value = forms.FloatField(
+        min_value=0.0,
+        max_value=2.0,
+        initial=1.0,
+        disabled=True,
+        widget=forms.NumberInput(attrs={"type": "range", "step": 0.01}),
+    )
+
+    video_saturation_boolean = forms.BooleanField(required=False)
+
+    video_saturation_value = forms.FloatField(
+        min_value=0.0,
+        max_value=3.0,
+        initial=1,
+        disabled=True,
+        widget=forms.NumberInput(attrs={"type": "range", "step": 0.01}),
+    )
+
+    lut_list = (
+        ("none", "None"),
+        ("Rec709_Fujifilm_3510_D65.cube", "Fujifilm 3510 (Juan Melara)"),
+        ("Rec709_Kodak_2383_D65.cube", "Kodak 2383 (Juan Melara)"),
+        ("Rec709_Kodak_2393_D65.cube", "Kodak 2393 (Juan Melara)"),
+        ("LBK-K-Tone_33.cube", "The new K-Tone (Frank Glencairn)"),
+        ("LBK-K-Tone-Intense_33.cube", "New K-Tone intense (Frank Glencairn)"),
+        (
+            "LBK-K-Tone-Intense_BlackLift_33.cube",
+            "New K-Tone intense - raised blacks (Frank Glencairn)",
+        ),
+    )
+
+    video_lut = forms.ChoiceField(choices=lut_list)
+
+    audio_volume = forms.FloatField(
+        min_value=AUDIO_VOLUME_MIN,
+        max_value=AUDIO_VOLUME_MAX,
+        initial=AUDIO_VOLUME_INITIAL,
+        widget=forms.NumberInput(attrs={"type": "range", "step": 0.01}),
+    )
+
+    # Equalizer presets
+    equalizer_presets = (
+        ("custom", "Custom"),
+        ("eq_hi_end", "Dialog - Clean add hi end"),
+        ("eq_dialog_female_lav", "Dialog - Female lav mic fixer"),
+        ("eq_dialog_male_lav", "Dialog - Male lav finisher"),
+        ("eq_dialog_male", "Dialog - Male"),
+        ("eq_telephone", "General - Telephone"),
+        ("eq_music_top_end_boost", "Music Master - Top end Boost"),
+    )
+
+    equalizer_preset = forms.ChoiceField(choices=equalizer_presets, initial="custom")
+
+    # Equalizer
+    eq_ten_band_1 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_2 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_3 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_4 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_5 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_6 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_7 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_8 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_9 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
+
+    eq_ten_band_10 = forms.FloatField(
+        min_value=-20,
+        max_value=20,
+        initial=0,
+        widget=forms.NumberInput(
+            attrs={"type": "range", "orient": "vertical", "step": 0.1}
+        ),
+    )
 
 
 def video_render_queue():
@@ -121,22 +299,92 @@ def check_audio_value(audio_bitrate):
         raise ValueError
 
 
-def video_filters(mirror, green_outlines, gaussian_blur):
+def video_filters(
+    mirror,
+    green_outlines,
+    frame_interpolation,
+    gaussian_blur,
+    video_brightness_boolean,
+    video_brightness_value,
+    video_contrast_boolean,
+    video_contrast_value,
+    video_saturation_boolean,
+    video_saturation_value,
+):
     filters = []
     if mirror == True:
         filters.append(("hflip", {}))
     if green_outlines == True:
         filters.append(("sobel", {}))
         filters.append(("eq", {"saturation": 0.2}))
+    if frame_interpolation == True:
         filters.append(("tmix", {"frames": 15}))
     if gaussian_blur == True:
         filters.append(
             ("gblur", {"sigma": 1}),
         )
+    if video_brightness_boolean == True and -1.0 <= video_brightness_value <= 1.0:
+        filters.append(("eq", {"brightness": video_brightness_value}))
+    if video_contrast_boolean == True and -1.0 <= video_contrast_value <= 2.0:
+        filters.append(("eq", {"contrast": video_contrast_value}))
+    if video_saturation_boolean == True and 0.0 <= video_saturation_value <= 3.0:
+        filters.append(("eq", {"saturation": video_saturation_value}))
     return filters
 
 
-# Nvidia RTX 30/40 Series supports 5 simultaneous encoding sessions
+def eq_ten_band(
+    band_1, band_2, band_3, band_4, band_5, band_6, band_7, band_8, band_9, band_10
+):
+    equalizer = []
+    equalizer.append(("equalizer", {"f": 31.5, "t": "o", "w": "1", "g": band_1}))
+    equalizer.append(("equalizer", {"f": 63, "t": "o", "w": "1", "g": band_2}))
+    equalizer.append(("equalizer", {"f": 125, "t": "o", "w": "1", "g": band_3}))
+    equalizer.append(("equalizer", {"f": 250, "t": "o", "w": "1", "g": band_4}))
+    equalizer.append(("equalizer", {"f": 500, "t": "o", "w": "1", "g": band_5}))
+    equalizer.append(("equalizer", {"f": 1000, "t": "o", "w": "1", "g": band_6}))
+    equalizer.append(("equalizer", {"f": 2000, "t": "o", "w": "1", "g": band_7}))
+    equalizer.append(("equalizer", {"f": 4000, "t": "o", "w": "1", "g": band_8}))
+    equalizer.append(("equalizer", {"f": 8000, "t": "o", "w": "1", "g": band_9}))
+    equalizer.append(("equalizer", {"f": 16000, "t": "o", "w": "1", "g": band_10}))
+    return equalizer
+
+
+def audio_filters(
+    audio_volume,
+    band_1,
+    band_2,
+    band_3,
+    band_4,
+    band_5,
+    band_6,
+    band_7,
+    band_8,
+    band_9,
+    band_10,
+):
+    audio_filters = []
+    if AUDIO_VOLUME_MIN <= audio_volume <= AUDIO_VOLUME_MAX:
+        audio_filters.append(("volume", {"volume": audio_volume}))
+    else:
+        raise ValueError
+    audio_filters.extend(
+        eq_ten_band(
+            band_1,
+            band_2,
+            band_3,
+            band_4,
+            band_5,
+            band_6,
+            band_7,
+            band_8,
+            band_9,
+            band_10,
+        )
+    )
+    return audio_filters
+
+
+# Need to check if an audio track exists
 def video_convert(
     input_name,
     video_file,
@@ -144,7 +392,8 @@ def video_convert(
     video_value,
     video_preset,
     audio_value,
-    custom_filters,
+    video_filters,
+    audio_filters,
 ):
     local_video_save(f"{input_name}", video_file)
     input = ffmpeg.input(f"uploaded_videos/{input_name}")
@@ -154,17 +403,14 @@ def video_convert(
         height=check_video_resolution_widescreen(video_resolution),
     )
 
-    for filter_name, filter_params in custom_filters:
-        print(len(custom_filters))
-        print(filter_name, filter_params)
-        print(len(filter_params))
+    for filter_name, filter_params in video_filters:
         video = video.filter(filter_name, **filter_params)
 
-        # .filter("sobel")
-        # .filter("eq", saturation=0.2)
-        # .filter("tmix", frames=15)
-        # .filter("hflip")
     audio = input.audio
+
+    for filter_name, filter_params in audio_filters:
+        audio = audio.filter(filter_name, **filter_params)
+
     ffmpeg.output(
         video,
         audio,
@@ -187,21 +433,44 @@ async def conversion(request):
     if request.method == "POST":
         form = ConvertVideo(request.POST, request.FILES)
         if form.is_valid():
-            video_convert(
-                form.cleaned_data["input_file"],
-                request.FILES["input_file"],
-                form.cleaned_data["video_resolution"],
-                check_video_value(
-                    form.cleaned_data["video_mode"],
-                    form.cleaned_data["video_qp"],
-                    form.cleaned_data["video_bitrate"],
-                ),
-                form.cleaned_data["video_preset"],
-                check_audio_value(form.cleaned_data["audio_bitrate"]),
-                video_filters(
-                    form.cleaned_data["video_flip"],
-                    form.cleaned_data["green_outlines"],
-                    form.cleaned_data["gaussian_blur"],
-                ),
-            )
+            try:
+                video_convert(
+                    form.cleaned_data["input_file"],
+                    request.FILES["input_file"],
+                    form.cleaned_data["video_resolution"],
+                    check_video_value(
+                        form.cleaned_data["video_mode"],
+                        form.cleaned_data["video_qp"],
+                        form.cleaned_data["video_bitrate"],
+                    ),
+                    form.cleaned_data["video_preset"],
+                    check_audio_value(form.cleaned_data["audio_bitrate"]),
+                    video_filters(
+                        form.cleaned_data["video_flip"],
+                        form.cleaned_data["green_outlines"],
+                        form.cleaned_data["frame_interpolation"],
+                        form.cleaned_data["gaussian_blur"],
+                        form.cleaned_data["video_brightness_boolean"],
+                        form.cleaned_data["video_brightness_value"],
+                        form.cleaned_data["video_contrast_boolean"],
+                        form.cleaned_data["video_contrast_value"],
+                        form.cleaned_data["video_saturation_boolean"],
+                        form.cleaned_data["video_saturation_value"],
+                    ),
+                    audio_filters(
+                        form.cleaned_data["audio_volume"],
+                        form.cleaned_data["eq_ten_band_1"],
+                        form.cleaned_data["eq_ten_band_2"],
+                        form.cleaned_data["eq_ten_band_3"],
+                        form.cleaned_data["eq_ten_band_4"],
+                        form.cleaned_data["eq_ten_band_5"],
+                        form.cleaned_data["eq_ten_band_6"],
+                        form.cleaned_data["eq_ten_band_7"],
+                        form.cleaned_data["eq_ten_band_8"],
+                        form.cleaned_data["eq_ten_band_9"],
+                        form.cleaned_data["eq_ten_band_10"],
+                    ),
+                )
+            except (NotImplementedError, ValueError):
+                return render(request, "error.html")
         return render(request, "test.html")
