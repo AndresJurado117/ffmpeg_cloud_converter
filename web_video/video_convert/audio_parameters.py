43e5ac4505b2acc9dd2c47b7e8c13de8c96a44d6
Andres <xxjuanrocksxx@hotmail.com>
2024-04-17 12:51:42 -0500

    Changed code structure, added LUTS and tech photos

--- web_video/video_convert/audio_parameters.py
+++
@@ -1,5 +0,0 @@
-def check_audio_value(audio_bitrate: int, bitrate_min: int, bitrate_max: int) -> int:
-    if bitrate_min <= audio_bitrate <= bitrate_max:
-        return audio_bitrate * 1000
-    else:
-        raise ValueError
--- web_video/video_convert/ffmpeg_exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidVideoFileType(Exception):
-    """
-    Raised when the file is not a video file
-    """
-
-    pass
